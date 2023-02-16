8113d217137d6634c01a1c582f79668a6636a50a
Christian Pieczewski <christian@pieczewski.de>
2023-02-16 23:08:28 +0100

    Make Greeting resources reactive

--- src/main/java/de/pieczewski/qute/GreetingResource.java
+++ src/main/java/de/pieczewski/qute/GreetingResource.java
@@ -1,5 +1,6 @@
 package de.pieczewski.qute;
 
+import io.smallrye.mutiny.Uni;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,7 +11,7 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public Uni<String> hello() {
+        return Uni.createFrom().item("Hello from RESTEasy Reactive");
     }
 }
